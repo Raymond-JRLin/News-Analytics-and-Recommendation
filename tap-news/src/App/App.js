a4714185e342f58a83ae7c04f061bb6553d0e7f9
Junrui Lin <raymond.jr.lin@gmail.com>
2018-07-16 17:10:47 -0400

    update

--- tap-news/src/App/App.js
+++ tap-news/src/App/App.js
@@ -2,8 +2,10 @@ import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
 
 import React from 'react';
-import logo from '../../public/logo.png';
+import logo from '../pictures/logo.png';
 import './App.css';
+
+import NewsPanel from '../NewsPanel/NewsPanel';
 
 class App extends React.Component {
   render() {
@@ -11,7 +13,7 @@ class App extends React.Component {
       <div>
           <img src={logo} className="logo" alt="logo" />
           <div className="container">
-
+            <NewsPanel />
           </div>
       </div>
     );
