9ec7d4e42cfb188a79ece99d0ec29cb6eae43068
Junrui Lin <raymond.jr.lin@gmail.com>
2018-07-16 20:33:18 -0400

    update

--- web_server/server/app.js
+++ web_server/server/app.js
@@ -2,8 +2,8 @@
 var express = require('express');
 var path = require('path');
 
-
 var index = require('./routes/index');
+var news = require('./routes/news');
 
 var app = express();
 
@@ -14,6 +14,7 @@ app.set('view engine', 'jade');
 app.use('/static', express.static(path.join(__dirname, '../client/build/static/')));
 
 app.use('/', index);
+app.use('/news', news);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
