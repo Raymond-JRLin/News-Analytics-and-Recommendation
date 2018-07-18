1ed5338f4f068619c4df708e8d14ff55f513e863
Junrui Lin <raymond.jr.lin@gmail.com>
2018-07-17 20:42:55 -0400

    cloudAMQP test

---
+++ backend_server/utils/cloudAMQP_client_test.py
@@ -0,0 +1,19 @@
+from cloudAMQP_client import CloudAMQPClient
+
+CLOUDAMQP_URL = "amqp://zelyuksr:XiT0b20lFDZF4rzuSPjUIHtnxOnWKfqZ@rhino.rmq.cloudamqp.com/zelyuksr"
+
+TEST_QUEUE_NAME = "test"
+
+def test_basic():
+    client = CloudAMQPClient(CLOUDAMQP_URL, TEST_QUEUE_NAME)
+
+    # construct a message
+    sentMsg = {"test": "demo"}
+    client.sendMessage(sentMsg)
+    client.sleep(10)
+    receivedMsg = client.getMessage()
+    assert sentMsg == receivedMsg
+    print 'test_basic passed!'
+
+if __name__ == '__main__':
+    test_basic()
