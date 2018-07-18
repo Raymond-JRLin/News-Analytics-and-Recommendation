77ca2806ccb12bc82e742d6f3027e45777a23e88
Junrui Lin <raymond.jr.lin@gmail.com>
2018-07-18 16:21:46 -0400

    update after route

--- web_server/client/src/index.js
+++ web_server/client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App/App';
+import ReactDom from 'react-dom';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+
+ReactDom.render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
