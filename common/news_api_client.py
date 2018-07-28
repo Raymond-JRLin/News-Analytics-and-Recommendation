b1981eb0d2d6b1cdfb6c32796df408f6f6fa607b
Junrui Lin <raymond.jr.lin@gmail.com>
2018-07-28 16:33:26 -0400

    update

--- common/news_api_client.py
+++ common/news_api_client.py
@@ -1,35 +1,34 @@
 import requests
+from json import loads
 
 NEWS_API_ENDPOINT = 'https://newsapi.org/v1/'
 NEWS_API_KEY = '1ac0dcf940dc46e99aaf537ad0f5a287'
+
 ARTICLES_API = 'articles'
 
-DEFAULT_SOURCES = 'cnn'
+CNN = 'cnn'
 
+DEFAULT_SOURCES = [CNN]
 SORT_BY_TOP = 'top'
 
-def buildUrl(end_point=NEWS_API_ENDPOINT, api_name=ARTICLES_API):
-    '''
-    get url
-    '''
-    return end_point + api_name
+def _buildUrl(endPoint=NEWS_API_ENDPOINT, apiName=ARTICLES_API):
+    return endPoint + apiName
 
-def getNewsFromSource(sources=DEFAULT_SOURCES, sort_by=SORT_BY_TOP):
+def getNewsFromSource(sources=DEFAULT_SOURCES, sortBy=SORT_BY_TOP):
     articles = []
 
     for source in sources:
         payload = {'apiKey':NEWS_API_KEY,
                    'source':source,
-                   'sortBy':sort_by}
+                   'sortBy':sortBy}
 
-        response = requests.get(buildUrl(), params=payload)
-        res_json = loads(response.context)
+        response = requests.get(_buildUrl(), params=payload)
+        res_json = loads(response.content.decode('utf-8'))
 
-        # Extract info from response
+        # Extract news from response
         if (res_json is not None and
             res_json['status'] == 'ok' and
             res_json['source'] is not None):
-
             # populate news source in each articles
             for news in res_json['articles']:
                 news['source'] = res_json['source']
--- common/news_api_client_test.py
+++ common/news_api_client_test.py
@@ -4,10 +4,9 @@ def test_basic():
     news = client.getNewsFromSource()
     print news
     assert len(news) > 0
-    news = client.getNewsFromSource(sources=['cnn'], sort_by='top')
+    news = client.getNewsFromSource(sources=['cnn'], sortBy='top')
     assert len(news) > 0
     print 'test_basic passed!'
 
 if __name__ == "__main__":
     test_basic()
-    
