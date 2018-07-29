8c507a2d66b9e4973afda28689ba07e503507eef
Junrui Lin <raymond.jr.lin@gmail.com>
2018-07-29 19:11:12 -0400

    update more news source

--- news_pipeline/news_monitor.py
+++ news_pipeline/news_monitor.py
@@ -21,7 +21,17 @@ SCRAPE_NEWS_TASK_QUEUE_URL = "amqp://zelyuksr:XiT0b20lFDZF4rzuSPjUIHtnxOnWKfqZ@r
 SCRAPE_NEWS_TASK_QUEUE_NAME = "tap-news-scrape-news-task-queue"
 
 NEWS_SOURCES = [
-    'cnn'
+    'bbc-news',
+    'bbc-sport',
+    'bloomberg',
+    'cnn',
+    'entertainment-weekly',
+    'espn',
+    'ign',
+    'techcrunch',
+    'the-new-york-times',
+    'the-wall-street-journal',
+    'the-washington-post'
 ]
 
 redis_client = redis.StrictRedis(REDIS_HOST, REDIS_PORT)
