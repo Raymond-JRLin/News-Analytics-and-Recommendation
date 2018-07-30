a2868a0c7d223e7bd8002c8a76c98b0e1b979568
Junrui Lin <raymond.jr.lin@gmail.com>
2018-07-29 20:12:21 -0400

    update for pagination

--- web_server/server/rpc_client/rpc_client.js
+++ web_server/server/rpc_client/rpc_client.js
@@ -15,8 +15,16 @@ function add(a, b, callback) {
   });
 }
 
-
+// Get news summaries for a user
+function getNewsSummariesForUser(user_id, page_num, callback) {
+  client.request('getNewsSummariesForUser', [user_id, page_num], function(err, error, response) {
+    if (err) throw err;
+    console.log(response);
+    callback(response);
+  });
+}
 
 module.exports = {
-  add : add
+  add : add,
+  getNewsSummariesForUser : getNewsSummariesForUser
 }
